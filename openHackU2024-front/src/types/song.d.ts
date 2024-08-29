ec872ffb286fcd5acf7cc998d2f370b804b5b57a
karasuneo <yusei4520030401@gmail.com>
2024-08-29 14:29:44 +0900

    feat: デモデータを追加

--- openHackU2024-front/src/types/song.d.ts
+++ openHackU2024-front/src/types/song.d.ts
@@ -1,68 +1,67 @@
-import { ErrorResponse } from 'react-router-dom';
+import { ErrorResponse } from "react-router-dom";
 export type AccountAlinePostRequest = {
-    spotify_data: SpotifyData;
-    display_name: string;
-    user_id: string;
+  spotify_data: SpotifyData;
+  display_name: string;
+  user_id: string;
 };
 
 export type SongData = {
-    song: string;
-    artist: string;
+  song: string;
+  artist: string;
 };
-  
+
 export type SpotifyData = {
-    [playlistName: string]: SongData[];
+  [playlistName: string]: SongData[];
 };
 
 export type AccountAlinePostResponse = {
-    states: string;
+  states: string;
 };
-  
+
 export type RoomAccessPostRequest = {
-    pass: string;
-    display_name: string;
-    user_id: string;
+  pass: string;
+  display_name: string;
+  user_id: string;
 };
-  
+
 export type RoomAccessPostResponse = {
-    states: string;
-    display_names: DisplayNames[];
+  states: string;
+  display_names: DisplayNames[];
 };
 
 export type RoomJoinPostRequest = {
-    pass: string;
-    display_name: string;
-    user_id: string;
+  pass: string;
+  display_name: string;
+  user_id: string;
 };
 
 export type RoomJoinPostResponse = {
-    states: string;
+  states: string;
 };
-  
+
 export type RoomGetPostRequest = {
-    pass: string;
-    display_name: string;
-    user_id: string;
+  pass: string;
+  display_name: string;
+  user_id: string;
 };
 
 export type RoomGetPostResponse = {
-    display_names: display_names[];
-    song_data: song_data[];
+  display_names: display_names[];
+  song_data: song_data[];
 };
 
 type DisplayNames = {
-    display_name: string;
-    user_id: string;
+  display_name: string;
+  user_id: string;
 };
 
 type SongData = {
-    song_title : string;
-    song_artist : string;
-    overlap : number;
+  song_title: string;
+  song_artist: string;
+  overlap: number;
 };
 
-export type ErrorResponse = {  
-    states: string;
-    error : string;
+export type ErrorResponse = {
+  states: string;
+  error: string;
 };
-    
