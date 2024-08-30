cff4922b47b4e6c6c6160fc24d1b75821e518b06
karasuneo <yusei4520030401@gmail.com>
2024-08-30 12:35:03 +0900

    chore:

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
+
+export default config;
