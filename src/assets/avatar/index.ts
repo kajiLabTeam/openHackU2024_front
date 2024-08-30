72faeff26116092b3b161339d5a903f9eccc8cf9
karasuneo <yusei4520030401@gmail.com>
2024-08-30 16:26:15 +0900

    image:

---
+++ src/assets/avatar/index.ts
@@ -0,0 +1,19 @@
+import avatarDefault from "@/assets/avatar.svg";
+import avatar1 from "./01.png";
+import avatar2 from "./02.png";
+import avatar3 from "./03.png";
+import avatar4 from "./04.png";
+import avatar5 from "./05.png";
+import avatar6 from "./06.png";
+import avatar7 from "./07.png";
+
+export const AVATARS = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+];
+export const AVATAR_DEFAULT = avatarDefault;
